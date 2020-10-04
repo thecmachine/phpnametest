e2769c97b6633d342c3bc7d359cd5c9557e9741e
Clint Lauby <thecmachine05@gmail.com>
2020-10-04 10:34:13 -0400

    ready

--- index.php
+++ index.php
@@ -1,26 +1,29 @@
-<html>
+
 <?php 
-  print '<h1>Hello</h1>';
+  header('Content-Type: application/json; charset=utf-8');
+  print 'Hello';
   // $names = str_getcsv('names.csv');
   $names = array_map('str_getcsv', file('names.csv'));
   
+  //remove headers 
   array_shift($names);
+
   //sort array by parent
-  print_r( $names );
-  print '<br>';  
+  // print_r( $names );
+  // print '<br>';  
   function sortByOrder($a, $b) {
     return $a['1'] - $b['1'];
   }
-
   usort($names, 'sortByOrder' );
-  print_r( $names );
-  print '<br>';  
+  // print_r( $names );
+  // print '<br>';  
+  
 // basic name key
   foreach($names as $key => $name){
-    print '<br>';
-    print_r($name[0] . " name: " . $name[2] . " parent ID: " . $name[1]);
+    // print '<br>';
+    // print_r($name[0] . " name: " . $name[2] . " parent ID: " . $name[1]);
   }
-  print '<br>';
+  // print '<br>';
 
   $namesToJson = array();
   // $currentParent = 0;
@@ -29,7 +32,7 @@
 
     $namesToJson[$name[0]] = array(
       'id' => $name[0],
-      'parent_id' => $name[1],
+      // 'parent_id' => $name[1],
       'name' => $name[2],
       'children' => array(),
     );
@@ -37,7 +40,7 @@
       if($name[0] == $copyName[1]){
         $namesToJson[$name[0]]['children'][] = array(
           'id' => $copyName[0],
-          'parent_id' => $name[1],
+          // 'parent_id' => $name[1],
           'name' => $copyName[2],
           'children' => array(),
         );
@@ -48,12 +51,35 @@
    //todo do something with $currentParent 
   }
 
+  //master copy
+  $masterCopy = array();
+  foreach($namesToJson as $keyCopy => $nameCopy){
+    // print_r($nameCopy);
+    // print '<br>';
+    $masterCopy[] = array(
+      'id' => $nameCopy['id'],
+      'name' => $nameCopy['name'],
+      'children' => $nameCopy['children'],
+    );
+    // print_r($nameCopy['id'] . " name: " . $nameCopy['name'] . " Children:");
+    foreach($nameCopy['children'] as $kidKey => $kid){
+      // print '<br>';
+
+      // print_r("KID".$kid['id'] . " KIDname: " . $kid['name'] . " Children:");
+    }
+  }
+  // print '<br><br/>';
+  // print_r($masterCopy);
+  // print '<br><br/>';
+  print_r(json_encode($masterCopy, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+  // print '<br><br/>';
+  
   //todo compress array nest children up, array_lift? 
-  print_r(json_encode($namesToJson, JSON_PRETTY_PRINT));
-  print "<br>";
+  // print_r(json_encode($namesToJson, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
+  // print "<br>";
+
   //json broken pretty print
-  print_r(json_encode($names, JSON_PRETTY_PRINT));
+  // print_r(json_encode($names, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
 
 
   ?>
-  </html>
